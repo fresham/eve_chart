7002fc6283070768fb60d2015683df7926b1808f
Cody Frazer <frazercr@gmail.com>
2018-05-06 11:39:55 -0400

    Skip view and controller specs by default

--- config/application.rb
+++ config/application.rb
@@ -15,5 +15,10 @@ module EvePi
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    config.generators do |g|
+      g.controller_specs  false
+      g.view_specs        false
+    end
   end
 end
